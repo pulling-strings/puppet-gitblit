14c4a03f13888ae9359d7d0c3d566ec1ebd196d2
ronen <narkisr@gmail.com>
2015-02-23 16:52:25 +0200

    fixing template new lines and making resources unique

--- manifests/user.pp
+++ manifests/user.pp
@@ -6,24 +6,12 @@ define gitblit::user(
 
   validate_string($password)
 
-  concat { '/opt/gitblit/data/users.conf':
-    ensure => present,
-  }
+  ensure_resource('concat','/opt/gitblit/data/users.conf',{ensure => present})
 
-  concat::fragment {'users.conf' :
+  concat::fragment {"add user ${name}" :
     target  => '/opt/gitblit/data/users.conf',
     content => template('gitblit/user.erb'),
     order   => '00',
     require => Archive["gitblit-${::gitblit::version}"]
   } ~> Service['gitblit']
-
-  #
-  # file { '/opt/gitblit/data/users.conf':
-  #   ensure  => file,
-  #   mode    => '0644',
-  #   content => template('gitblit/users.conf.erb'),
-  #   owner   => root,
-  #   group   => root,
-  #   require => Archive["gitblit-${::gitblit::version}"]
-  # } ~> Service['gitblit']
 }
