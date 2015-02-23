d6c7e1583616c3f6c8e25e7decb3ea5734478e92
ronen <narkisr@gmail.com>
2015-02-23 18:48:20 +0200

    adding keys support

--- manifests/user.pp
+++ manifests/user.pp
@@ -1,16 +1,26 @@
 # Setting up gitblit users
 define gitblit::user(
   $password = undef,
-  $roles = []
+  $roles = [],
+  $keys  = []
 ) {
 
   validate_string($password)
 
   ensure_resource('concat','/opt/gitblit/data/users.conf',{ensure => present})
 
+  ensure_resource('concat',"/opt/gitblit/data/ssh/${name}.keys",{ensure => present})
+
   concat::fragment {"add user ${name}" :
     target  => '/opt/gitblit/data/users.conf',
     content => template('gitblit/user.erb'),
+    order   => '00',
+    require => Archive["gitblit-${::gitblit::version}"]
+  } ~> Service['gitblit']
+
+  concat::fragment {"add keys for ${name}" :
+    target  => "/opt/gitblit/data/ssh/${name}.keys",
+    content => template('gitblit/keys.erb'),
     order   => '00',
     require => Archive["gitblit-${::gitblit::version}"]
   } ~> Service['gitblit']
