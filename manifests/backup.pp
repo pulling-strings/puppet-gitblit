37716cc81ac19129475ba4d1792302e64dc4153c
ronen <narkisr@gmail.com>
2015-03-25 22:29:11 +0200

    adding options

--- manifests/backup.pp
+++ manifests/backup.pp
@@ -3,20 +3,22 @@ class gitblit::backup(
   $bucket     = undef,
   $pass       = undef,
   $user       = undef,
-  $passphrase = undef
+  $passphrase = undef,
+  $options = ''
 ){
 
-  validate_string($bucket, $pass, $user, $passphrase)
+  validate_string($bucket, $pass, $user, $passphrase, $options)
 
   include barbecue
 
   backup::duply {'gitblit-repos':
-    source      => $::gitblit::config::repo_folder,
-    target      => $bucket,
-    target_pass => $pass,
-    target_user => $user,
-    passphrase  => $passphrase,
-    globs       => ''
+    source            => $::gitblit::config::repo_folder,
+    target            => $bucket,
+    target_pass       => $pass,
+    target_user       => $user,
+    passphrase        => $passphrase,
+    globs             => '',
+    dupliticy_options => $options
   }
 
   backup::duply::schedule {'gitblit-repos': }
