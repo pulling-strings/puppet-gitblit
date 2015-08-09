155324c61cbe2a88bab930cfe1f9860e34c89b0f
ronen <narkisr@gmail.com>
2015-08-09 15:20:11 +0300

    merging

---
+++ manifests/config.pp
@@ -0,0 +1,33 @@
+# Setting up gitblit config, $repo_folder should exist 
+class gitblit::config(
+  $repo_folder = '/var/gitblit/repos'
+){
+
+  validate_string($repo_folder)
+
+  if $repo_folder == '/var/gitblit/repos' {
+    file{['/var/gitblit','/var/gitblit/repos']:
+      ensure => directory,
+    }
+  }
+
+  Archive["gitblit-${::gitblit::version}"] -> Editfile <||>
+
+  Editfile <||> ~> Service['gitblit']
+
+  editfile::config { 'repo folder':
+    ensure => " ${repo_folder}",
+    entry  => 'git.repositoriesFolder ',
+    path   => '/opt/gitblit/data/gitblit.properties',
+    quote  => false
+  }
+
+  editfile { 'local host unbind':
+    ensure   => absent,
+    provider => regexp,
+    path     => '/opt/gitblit/data/gitblit.properties',
+    match    => '/server.httpsBindInterface.*/',
+    notify   => Service['gitblit']
+  }
+
+}
